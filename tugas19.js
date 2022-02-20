740c8d81c51850dc4219091ddc3a727964b8092c
faldyantono <96801457+faldyantono@users.noreply.github.com>
2022-02-21 03:19:21 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,23 @@
+console.log("                                         ");
+
+function tugas() {
+  var x = "semangat...";
+  console.log(x.repeat(5));
+}
+tugas();
+
+console.log("                                         ");
+
+function bintang() {
+  var b = "*"
+  console.log(b.repeat(5));
+  console.log(b.repeat(30));
+  console.log(b.repeat(60));
+  console.log(b.repeat(90));
+  console.log(b.repeat(130));
+  console.log(b.repeat(90));
+  console.log(b.repeat(60));
+  console.log(b.repeat(30));
+  console.log(b.repeat(5));
+}
+bintang();
